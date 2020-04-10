34ec26675dea58d7cd33a928daacb1148c742cf2
kartik2097 <31465803+kartik2097@users.noreply.github.com>
2020-04-10 13:02:15 +0530

    updated homepage

--- Home.js
+++ Home.js
@@ -24,3 +24,72 @@ function openNav() {
   function closeNav() {
     document.getElementById("myNav").style.height = "0%";
   } 
+
+
+  $(document).ready(function(){
+    $('.Whatforyou').waypoint(function(direction){
+      $('.Whatforyou').addClass('animated lightSpeedIn')
+    },{
+      offset: '300px'
+    })
+
+    $('.flip-card-ani1').waypoint(function(direction){
+      $('.flip-card-ani1').addClass('animated fadeInLeft')
+    },{
+      offset: '50%'    
+    })
+
+    $('.flip-card-ani1').waypoint(function(direction){
+      $('.flip-card-ani1').addClass('animated fadeInLeft')
+    },{
+      offset: '50%'    
+    })
+
+    $('.flip-card-2-ani').waypoint(function(direction){
+      $('.flip-card-2-ani').addClass('animated fadeInRight')
+    },{
+      offset: '50%'    
+    })
+
+    $('.flip-card-2-ani').waypoint(function(direction){
+      $('.flip-card-2-ani').addClass('animated fadeInRight')
+    },{
+      offset: '50%'    
+    })
+
+
+    $('.Student-animate').waypoint(function(direction){
+      $('.Student-animate').addClass('animated fadeInRight')
+    },{
+      offset: '70%'    
+    })
+    $('.Student-Img-animate').waypoint(function(direction){
+      $('.Student-Img-animate').addClass('animated fadeInUp')
+    },{
+      offset: '70%'    
+    })
+    // $('.Soc-animate').waypoint(function(direction){
+    //   $('.Soc-animate').addClass('animated fadeInRight')
+    // },{
+    //   offset: '50%'    
+    // })
+    // $('.Tech-animate').waypoint(function(direction){
+    //   $('.Tech-animate').addClass('animated fadeInRight')
+    // },{
+    //   offset: '60%'    
+    // })
+    // $('.Manage-animate').waypoint(function(direction){
+    //   $('.Manage-animate').addClass('animated fadeInRight')
+    // },{
+    //   offset: '70%'    
+    // })
+    // $('.resp-animate').waypoint(function(direction){
+    //   $('.resp-animate').addClass('animated fadeInRight')
+    // },{
+    //   offset: '80%'    
+    // })
+
+
+
+
+  })
